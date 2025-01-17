3335d4c7ae39f45414aa150de7821fc5b82c2cf9
magmueller <mamagnus00@gmail.com>
2025-01-16 19:29:21 -0800

    Calculate scores for result folders

--- build_repo.sh
+++ build_repo.sh
@@ -2,7 +2,7 @@
 
 # go to location of this script
 # cd $(dirname $0)
-# # remove browser-use folder if it exists
+# remove browser-use folder if it exists
 # rm -rf browser-use
 
 # Simple test script for browser-use Gemini branch
@@ -17,8 +17,10 @@ git checkout dev
 
 # Setup environment and build
 # echo "Setting up environment and building"
-uv venv
+cd ..
+uv venv --python=3.11
 source .venv/bin/activate
+cd browser-use
 uv pip install build 
 python -m build
 
@@ -34,6 +36,7 @@ playwright install
 # copy test.py to browser-use
 #echo "Copying test.py to browser-use"
 cd ..
+source .venv/bin/activate
 #cp test.py browser-use/test.py
 #cd browser-use
 
@@ -50,3 +53,5 @@ cd ..
 
 # c
 # i browser-use
+
+# a .venv
--- calculate_current_score.py
+++ calculate_current_score.py
@@ -1,19 +1,33 @@
 import json
 import os
 
-folder = "results/examples-browser-use"
 
-success_count = 0
-total_count = 1
-print(f"Total tasks: {len(os.listdir(folder))}")
-for subfolder in os.listdir(folder):
-    for file in os.listdir(os.path.join(folder, subfolder)):
-        if file.endswith("task_result.json"):
-            print(f"Processing {subfolder}")
-            with open(os.path.join(folder, subfolder, file), "r") as f:
-                data = json.load(f)
-            if data["success"] == "success":
-                success_count += 1
-            total_count += 1
+def get_score(folder):
+    success_count = 0
+    total_count = 1
+    print(f"Processing {folder}")
+    print(f"Total tasks: {len(os.listdir(folder))}")
+    for subfolder in os.listdir(folder):
+        # if not folder, skip
+        if not os.path.isdir(os.path.join(folder, subfolder)):
+            continue
+        for file in os.listdir(os.path.join(folder, subfolder)):
+            if file.endswith("task_result.json"):
+                with open(os.path.join(folder, subfolder, file), "r") as f:
+                    data = json.load(f)
+                if data["success"] == "success":
+                    success_count += 1
+                total_count += 1
 
-print(f"Success rate: {success_count / total_count:.2f}={success_count}/{total_count}")
+    print(
+        f"Success rate : {success_count / total_count:.2f}={success_count}/{total_count}"
+    )
+    print()
+
+
+main_folder = "results"
+print("Success rate for each folder:")
+for folder in os.listdir(main_folder):
+    if not os.path.isdir(os.path.join(main_folder, folder)):
+        continue
+    get_score(os.path.join(main_folder, folder))
